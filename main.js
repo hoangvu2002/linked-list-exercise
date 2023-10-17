59643d7f631c74de06dd511e225dab603b433eab
pdhoangvu <pdhoangvu@gmail.com>
2023-10-17 17:05:33 +0700

    Add append method for linked list factory

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
 function LinkedList() {
-    
+  const list = {
+    head: null,
+  };
+
+  const append = function(value) {
+    // eslint-disable-next-line no-use-before-define
+    const node = Node(value);
+    let tmp = list.head;
+    if (tmp===null) {
+      list.head = node;
+    } else {
+      while (tmp.nextNode !== null) {
+        tmp = tmp.nextNode;
+      };
+      tmp.nextNode = node;
+    };
+  }
+
+  return {
+    append, list,
+  }
 }
 
-function Node() {
-    
+function Node(value=null) {
+  // eslint-disable-next-line no-unused-vars
+  let nextNode;
+  return {
+    value,
+    nextNode: null,
+  };
 }
+
+const linkedList = LinkedList();
+linkedList.append('fuck');
+console.log(linkedList.list);
+linkedList.append('you');
+console.log(linkedList.list);
+linkedList.append('mfs');
+console.log(linkedList.list);
+linkedList.append('???');
+console.log(linkedList.list);
